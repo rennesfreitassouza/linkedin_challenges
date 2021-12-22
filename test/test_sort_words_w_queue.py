686d8a09d8e3aca3e34e74273b432e7fd3215720
rennesfreitassouza <rennesfreitas@gmail.com>
2021-12-22 08:09:03 -0300

    sort a string tests

---
+++ test/test_sort_words_w_queue.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.fixture(params=["orange ORANGE", "ORANGE orange", "banana ORANGE apple", "0A 0a", "0a 0A", "00A 00a", "00a 10A"])
+def string(request):
+    return request.param
+
+def test_prime_values(string):
+    from ans import SortAString
+
+    retorno = SortAString.sort_words_w_queue(string)
+    
+    assert retorno == SortAString.sort_words_linkedin(string), print(retorno)
