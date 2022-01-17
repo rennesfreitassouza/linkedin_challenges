35acc6d94d100406dffda2ab121c6ff128e3277d
rennesfreitassouza <rennesfreitas@gmail.com>
2022-01-17 09:20:06 -0300

    New docstring

--- Files_Python_Code_Challenges/IdentifyaPalindrome.py
+++ Files_Python_Code_Challenges/IdentifyaPalindrome.py
@@ -1,16 +1,20 @@
-
 import re
 
+
 def isaPalindrome(string):
-    '''https://www.linkedin.com/learning/python-code-challenges/identify-a-palindrome'''
+    '''https://www.linkedin.com/learning/python-code-challenges\
+/identify-a-palindrome'''
     letters = ''.join(re.findall(r'(?ai)[a-z]+', string))
-    
+
     # backwl = letters[::-1].lower()
     backwl = ''.join(reversed(letters))
     return backwl.lower() == letters.lower()
+
 
 def main():
     string = input("Type a word: ")
     return isaPalindrome(string)
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
